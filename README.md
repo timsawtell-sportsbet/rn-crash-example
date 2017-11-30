d89958699d2c5d5df5327164e157ea91fe93673a
Tim Sawtell <Tim.Sawtell@sportsbet.com.au>
2017-11-30 13:56:15 +1100

    changed some internal comments

--- README.md
+++ README.md
@@ -2,11 +2,11 @@ Clone repo
 
 yarn install
 
-Open Xcode, Run. All should be good.
+Open Xcode, Run. All should be good, you'll see 3 labels on the screen.
 
 Change the target (rncrashexample)'s scheme's Build Configuration from debug to release.
 
-Run the app and watch it crash.
+Run the app and you'll see that the child elements never make it across the bridge.
 
 Look in NativeView.m for details.
 
--- rncrashexample/ios/NativeView.m
+++ rncrashexample/ios/NativeView.m
@@ -20,14 +20,6 @@
     [self addSubview:label];
     i++;
   }
-  // make the app crash when in release mode.
-  // it will not crash  in debug mode becuase `simpleArray` has 3 elements in it
-  // in release mode it's an empty array. This is EXACTLY the point of raising the bug.
-
-  // Remove this array index accessor if you think maybe it's just empty on the first time into this method
-  // Unfortunately this setter is only ever called ONCE
-  NSString *crashMe = simpleArray[1];
-  NSLog(@"%@", crashMe);
 }
 
 @end
